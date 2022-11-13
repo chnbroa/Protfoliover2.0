507ba4f609d0b34bcd367964865d869fd37a190a
chnbroa <chnbroa011@naver.com>
2022-11-14 03:22:14 +0900

    2022-11-14

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 function LandingPage() {
 
   useEffect(() => {
-    axios.get('/api/hello')
+    axios.get('http://localhost:5000/api/hello')
       .then(response => console.log(response.data))
   }, [])
 
