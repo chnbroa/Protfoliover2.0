16c7da93f960ac39bcac8cabc62b9bb2c3a2b4c0
chnbroa <chnbroa011@naver.com>
2022-11-15 01:53:13 +0900

    22-11-15
    Proxy server + Concurrently
    
    24강

--- client/src/index.js
+++ client/src/index.js
@@ -7,11 +7,13 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-  <React.StrictMode>
-    <App />
 
-  </React.StrictMode>
+  <App />
+
+
 );
+
+//  <React.StrictMode> 는 랜더링 2번
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
